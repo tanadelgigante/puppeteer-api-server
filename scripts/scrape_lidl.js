043564c6603f1c0b13dd3a798a8c3b0a0a0b5730
Il Gigante <tanadelgigante@outlook.com>
2024-11-20 17:27:17 +0100

    call webhook

--- scripts/scrape_lidl.js
+++ scripts/scrape_lidl.js
@@ -1,11 +1,25 @@
+const axios = require('axios');
+
 module.exports = async (browser, url) => {
     const page = await browser.newPage();
     await page.goto(url);
-    
+
     const links = await page.$$eval('a.ods-tile__link', anchors => anchors.map(a => a.href));
 
     // Trasforma i link nel formato desiderato
     const formattedLinks = links.map(link => ({ link }));
 
-    return { results: { data: formattedLinks } };
+    // Costruisci il payload per il Webhook Agent
+    const payload = { results: { data: formattedLinks } };
+
+	const webhookUrl = 'http://192.168.188.121:4750/users/1/web_requests/16/3fb20a86-9111-4351-a585-0397c89a5c14';
+	// Chiamata al Webhook Agent di Huginn
+    try {
+        await axios.post(webhookUrl, payload);
+        console.log('Webhook chiamato con successo');
+    } catch (error) {
+        console.error('Errore nella chiamata al Webhook:', error);
+    }
+
+    return "OK";
 };
